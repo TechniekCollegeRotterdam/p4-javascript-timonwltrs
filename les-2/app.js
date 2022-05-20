dfef5f0e2554ef8484d70baa1d29ccb8e69cc8ef
timonwltrs <9012910@student.zadkine.nl>
2022-05-20 14:06:22 +0200

    bozevogel

--- les-2/app.js
+++ les-2/app.js
@@ -1,9 +1,31 @@
 //Oefening angry-birds
 
+// const bird = document.querySelector(".bird");
+// let px = 0;
+// bird.addEventListener("click", function(){
+//     px = px + 50;
+//     bird.style.left = px + "px";
+// })
+
 const bird = document.querySelector(".bird");
 let px = 0;
-bird.addEventListener("click", function(){
-    px = px + 50;
-    bird.style.left = px + "px";
-})
+window.addEventListener("keydown", function(event){
+    console.log(event.key);
 
+    if(event.key == "ArrowRight"){
+        px = px + 50;
+        bird.style.left = px + "px";
+    }
+    if(event.key == "ArrowLeft"){
+        px = px - 50;
+        bird.style.left = px + "px";
+    }
+    if(event.key == "ArrowUp"){
+        px = px + 50;
+        bird.style.bottom = px + "px";
+    }
+    if(event.key == "ArrowDown"){
+        px = px - 50;
+        bird.style.bottom = px + "px";
+    }
+})
