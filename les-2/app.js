38a553ddc30a2bfc0077c0104260659264930d99
timonwltrs <9012910@student.zadkine.nl>
2022-06-03 11:46:37 +0200

    opdracht 3

--- les-2/app.js
+++ les-2/app.js
@@ -1,31 +1,44 @@
 //Oefening angry-birds
-
-// const bird = document.querySelector(".bird");
-// let px = 0;
-// bird.addEventListener("click", function(){
-//     px = px + 50;
-//     bird.style.left = px + "px";
-// })
-
+//.is voor class
+//#is voor id
+// niks is voor html-tags
+//de image ophaald uit de html 
 const bird = document.querySelector(".bird");
 let px = 0;
-window.addEventListener("keydown", function(event){
-    console.log(event.key);
 
-    if(event.key == "ArrowRight"){
-        px = px + 50;
-        bird.style.left = px + "px";
+bird.addEventListener("click", function(){
+    //elke keer als ik op de bird klik, word er 50 opgeteld. 
+    px = px + 50;
+    //voeg styling toe aan je bird class met als property 
+    // left : 50px
+    bird.style.left = px + "px";
+})
+
+window.addEventListener("keydown", function(e){
+    console.log(e.key); 
+    if(e.key == "ArrowRight"){
+    px = px + 50;
+    bird.style.left = px + "px";
+    bird.src = "img/b-right.svg";
     }
-    if(event.key == "ArrowLeft"){
+
+    if(e.key == "ArrowLeft"){
         px = px - 50;
         bird.style.left = px + "px";
+        bird.src = "img/b-left.svg";
+        }
+
+    if(e.key == "ArrowUp"){
+            px = px + 50;
+            bird.style.bottom = px + "px";
+            bird.src = "img/b-up.svg";
+            }
+
+    if(e.key == "ArrowDown"){
+    px = px - 50;
+    bird.style.bottom = px + "px";
+    bird.src = "img/b-down.svg";
     }
-    if(event.key == "ArrowUp"){
-        px = px + 50;
-        bird.style.bottom = px + "px";
-    }
-    if(event.key == "ArrowDown"){
-        px = px - 50;
-        bird.style.bottom = px + "px";
-    }
+
+
 })
